7fe304d722a5c125b6df57bd293e3da26dbdfd9e
Wilson Siaw <wilsonsiawse@gmail.com>
2023-11-20 21:35:38 +0200

    Insert Card the component in to the root component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import Button from '../Button/Button';
+import Card from '../Card/Card';
 
 function App() {
   return (
-    <div>
+    <div className='body'>
       <Header 
       heading='devjobs'
       />
+      <Card />
       <Button 
       value='Load More'
       />
