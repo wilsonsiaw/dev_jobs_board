ba3b64cff6aa788d867eb8c2ae717a6195de49c6
Wilson Siaw <wilsonsiawse@gmail.com>
2023-11-20 17:16:48 +0200

    Add the Button component to the root component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header';
+import Button from '../Button/Button';
 
 function App() {
   return (
     <div>
       <Header 
-      heading='devjobs'/>
+      heading='devjobs'
+      />
+      <Button 
+      value='Load More'
+      />
     </div>
   )
 }
