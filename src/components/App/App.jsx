14934b335ad6bf637b7f0d5a97f08b005b392ac7
Wilson Siaw <wilsonsiawse@gmail.com>
2023-11-20 16:56:43 +0200

    Adjust file paths

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header 
+      heading='devjobs'/>
+    </div>
+  )
+}
+
+export default App
